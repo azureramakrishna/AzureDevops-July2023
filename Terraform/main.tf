f7f66aadfa3fc24f5d5d7824fe6c3d622e73d291
Ramakrishna <saanvikitmay2023@gmail.com>
2023-08-02 19:18:25 +0200

    Terraform Azure VM

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,9 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.61.0"
+    }
+  }
+}
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {
     #skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   }
 }
+
+# Terraform backend state
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "cloud-shell-storage-centralindia"
+    storage_account_name = "csg10032000825eeb72"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
@@ -18,4 +38,98 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "example" {
+  depends_on = [ azurerm_network_security_group.example ]
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = var.virtual_network_address
+}
+
+# Create subnet
+resource "azurerm_subnet" "example" {
+  depends_on = [ azurerm_network_security_group.example ]
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = var.subnet_address
+}
+
+# Create Public IP
+resource "azurerm_public_ip" "example" {
+  name                = var.public_ip_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+
+# Create Network security group
+resource "azurerm_network_security_group" "example" {
+  name                = var.network_security_group_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create Network interface
+resource "azurerm_network_interface" "example" {
+  depends_on = [ azurerm_public_ip.example, azurerm_subnet.example ]
+  name                = var.network_interface_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+# Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "example" {
+  depends_on = [ azurerm_network_security_group.example, azurerm_subnet.example ]
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+# Create Windows VM
+resource "azurerm_windows_virtual_machine" "example" {
+  depends_on = [ azurerm_network_interface.example ]
+  name                = var.virtual_machine_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = var.virtual_machine_size
+  admin_username      = var.adminUser
+  admin_password      = var.adminPassword
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    name                 = "${var.virtual_machine_name}-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -15,3 +15,59 @@ variable "storage_account_name" {
   description = "Name of the storage account"
   default     = "Terraformsa20230801"
 }
+
+variable "virtual_network_name" {
+  type        = string
+  description = "Name of the virtual network"
+  default     = "terraform-vnet"
+}
+
+variable "virtual_network_address" {
+  type    = list(any)
+  default = ["10.0.0.0/24"]
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "terraform-snet"
+}
+
+variable "subnet_address" {
+  type    = list(any)
+  default = ["10.0.0.0/24"]
+}
+
+variable "public_ip_name" {
+  type    = string
+  default = "terraform-pubip"
+}
+
+variable "network_security_group_name" {
+  type    = string
+  default = "terraform-nsg"
+}
+
+variable "network_interface_name" {
+  type    = string
+  default = "terraform-nic"
+}
+
+variable "virtual_machine_name" {
+  type    = string
+  default = "terraform-vm"
+}
+
+variable "virtual_machine_size" {
+  type    = string
+  default = "Standard_B2s"
+}
+
+variable "adminUser" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "adminPassword" {
+  type    = string
+  default = "Azuredevops@12345"
+}
