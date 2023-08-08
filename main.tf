e380a006ad3ed4d39ac660852c1c0f9b0f8e0b8b
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2023-08-08 18:35:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
-# Azure terraform provider version
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.68.0"
-    }
-  }
-}
-
-# Azure terraform provider
-provider "azurerm" {
-  features {}
-
-  client_id       = var.clientid
-  client_secret   = var.clientSecret
-  tenant_id       = var.tenantId
-  subscription_id = var.subscriptionId
-}
-
-# Terraform backend state
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-centralindia"
-    storage_account_name = "csg10032000825eeb72"
-    container_name       = "tfstate"
-    key                  = "storage.terraform.tfstate"
-  }
-}
-
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
